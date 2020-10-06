9342e31a3f4003c16866ca7851bfb497a533ecc3
Ekene N <ekenayy@gmail.com>
2020-10-06 15:56:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,10 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  
+  if hash[key]
+    puts
+  else 
+    puts 'sorry'
   # return the correct value using the hash and key parameters
 end
 

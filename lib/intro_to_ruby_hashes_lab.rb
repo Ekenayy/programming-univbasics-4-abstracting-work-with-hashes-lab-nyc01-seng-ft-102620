5cf23df43f137dd327cf4c6ded2b3050898fbe65
Ekene N <ekenayy@gmail.com>
2020-10-06 15:48:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  return new_hash = {"key" => value}
+  new_hash = []
+  return new_hash = {:key => value}
 end
 
 def read_from_hash(hash, key)
+  
   # return the correct value using the hash and key parameters
 end
 
